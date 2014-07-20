5d540c876effe6f7fa8ce353ea6e44c8c0ce3821
wideeyes <wideeyes@wideeyes.(none)>
2014-07-20 18:24:28 +0200

    tutorial. 'login/logout' part

--- app/views.py
+++ app/views.py
@@ -1,22 +1,65 @@
-from app import app
-from flask import render_template,  flash,  redirect
+from app import app, db, lm, oid
+from flask import render_template,  flash,  redirect, session, url_for, request, g
 from forms import LoginForm
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from models import User, ROLE_USER, ROLE_ADMIN
 
 @app.route('/login',  methods = ['GET',  'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login request for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me
+        return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
     return render_template('login.html', 
                            title = 'Sign In', 
                            form = form, 
                            providers = app.config['OPENID_PROVIDERS'])
+                        
+                        
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+                        
+@app.before_request
+def before_request():
+    g.user = current_user
+                        
+@oid.after_login
+def after_login(resp):
+    # check valid email
+    print resp.email
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please, try again.')
+        return redirect(url_for('login'))
+    
+    # login logic for signup or signin
+    user = User.query.filter_by(email = resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        # signup
+        user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+        
+    
 
 @app.route('/')
 @app.route('/index')
+@login_required #the users cant get this view if they are not logged in
 def index():
-    user = {'nickname': 'LongLong'}
+    user = g.user
     posts = [ # fake array of posts
              {
                  'author': {'nickname': 'John'}, 
@@ -31,3 +74,7 @@ def index():
                            title ='Home',  
                            user = user, 
                            posts = posts)
+                        
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
